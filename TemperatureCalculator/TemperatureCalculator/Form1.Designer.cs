0220356ecd58ec44a5d831583a4fdb698cd0387e
suthada sopab <96216472+suthada@users.noreply.github.com>
2021-12-16 12:08:06 +0700

    add f to c

--- TemperatureCalculator/TemperatureCalculator/Form1.Designer.cs
+++ TemperatureCalculator/TemperatureCalculator/Form1.Designer.cs
@@ -96,7 +96,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(132, 66);
             this.button2.TabIndex = 7;
-            this.button2.Text = "clen";
+            this.button2.Text = "clear";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
